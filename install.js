b3e5b3068616765d86a540984977b9f7265b83e5
owyr <143370246+owyr@users.noreply.github.com>
2023-08-31 11:46:53 +0800

    Update install.js

--- install.js
+++ install.js
@@ -1,1 +1,10 @@
-
+run()
+async function run() {
+  const fm = FileManager[module.filename.includes("Documents/iCloud~") ? "iCloud" : "local"]();
+  const filePath = fm.joinPath(fm.documentsDirectory(), `宝马小组件.js`);
+  const url = "https://cdn.jsdelivr.net/gh/owyr/bmw-widget@main/bmw-widget.js";
+  const content = await new Request(encodeURI(url)).load();
+  fm.write(filePath, content);
+  fm.remove(module.filename);
+  Safari.open(encodeURI(`scriptable:///run?scriptName=宝马小组件`));
+}
